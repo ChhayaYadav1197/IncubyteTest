80dde2c9633481e793f6145b13d96fcb64bfded4
Chhaya Yadav <chhaya@centilytics.com>
2020-06-25 09:35:44 +0530

    Allow the Add method to handle new lines between numbers (instead of commas)

--- StringCal/src/StringCalculator.java
+++ StringCal/src/StringCalculator.java
@@ -2,15 +2,19 @@ package src;
 
 public class StringCalculator {
 
-	int Add(String numbers) {
+	int Add(String numbers) throws Exception {
 		if (numbers.isEmpty())
 			return 0;
 		else {
 			String[] numberArray = numbers.split(",");
 			int addition = 0;
-			for(String s : numberArray)
-			{
-				addition+=Integer.parseInt(s) ;
+			for (String s : numberArray) {
+				if (s.isBlank() || s.startsWith("\n") || s.endsWith("\n"))
+					throw new NumberFormatException("Invalid Input");
+				for (String x : s.split("\n")) {
+					
+					addition += Integer.parseInt(x);
+				}
 			}
 			return addition;
 		}
--- StringCal/src/StringCalculatorTest.java
+++ StringCal/src/StringCalculatorTest.java
@@ -1,32 +1,49 @@
 package src;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
 
 	@Test
-	void testAddEmptyString() {
+	void testAddEmptyString() throws Exception {
 		StringCalculator cal = new StringCalculator();
 		assertEquals(0, cal.Add(""));
 	}
-	
+
 	@Test
-	void testAddSingleNumber() {
+	void testAddSingleNumber() throws Exception {
 		StringCalculator cal = new StringCalculator();
 		assertEquals(1, cal.Add("1"));
 	}
+
 	@Test
-	void testAddTwoNumbers() {
+	void testAddTwoNumbers() throws Exception {
 		StringCalculator cal = new StringCalculator();
 		assertEquals(3, cal.Add("1,2"));
 	}
-	
 
 	@Test
-	void testAddUnknowAmountOfNumbers() {
+	void testAddUnknowAmountOfNumbers() throws Exception {
 		StringCalculator cal = new StringCalculator();
 		assertEquals(19, cal.Add("1,2,3,4,4,5"));
+	}
+
+	@Test
+	void allowAddToHandleNewLineBetweenNumbers() throws Exception {
+		StringCalculator cal = new StringCalculator();
+		assertEquals(5, cal.Add("1\n2,2"));
+	}
+
+	@Test
+	void allowAddToHandleNewLineBetweenNumbersNotComma() {
+		StringCalculator cal = new StringCalculator();
+
+		assertThrows(NumberFormatException.class, () -> {
+			cal.Add("20,\n,");
+		});
+
 	}
 }
